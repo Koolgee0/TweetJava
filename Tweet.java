b4d9aad8f0e8e2df1aa4e46fecc9f9c02bc5b3c6
Koolgee0 <50880695+Koolgee0@users.noreply.github.com>
2020-10-04 23:02:49 -0400

    Add files via upload

---
+++ Tweet.java
@@ -0,0 +1,14 @@
+package com;
+
+public class Tweet {
+    private String message;
+    public Tweet (){
+
+    }
+    public void addLetter (char c){
+
+    }
+    public void deleteLetter(){
+
+    }
+}
